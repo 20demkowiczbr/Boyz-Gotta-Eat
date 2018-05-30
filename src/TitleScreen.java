2f864bd2156452cec313755ca673c98d4f1d17cb
20demkowiczbr <20demkowiczbr@fpsct.org>
2018-05-30 08:13:53 -0400

    Title Screen Works (still need button)

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,33 +1,51 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
 public class TitleScreen extends Application {
 	
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+	 
 	public void start(Stage stage) throws Exception{
 		
-		BorderPane layout = new BorderPane();
-		Scene scene = new Scene(layout, 680, 480);
+	
 		
-		Button startButton = new Button();
-		layout.setCenter(startButton);
-		
-		startButton.setText("Start");
-		
-		final Image titleScreen = new Image("asset/Stegosaurus.jpg");
+		final Image titleScreen = new Image("asset/bg.jpg");
 			
 			final ImageView titleNode = new ImageView();
 			titleNode.setImage(titleScreen);
 			
-			stage.setTitle("TBD");
 			stage.getIcons().add(titleScreen);
+			
+			Group node = new Group();
+			stage.setTitle("TBD");	
+			node.getChildren().addAll(titleNode);
+			Scene scene = new Scene(node, 680, 480);
+			stage.setScene(scene);
+				
+			GridPane pane = new GridPane();
+			pane.setPadding(new Insets(10, 10, 10, 10));
+	        pane.setMinSize(300, 300);
+	        pane.setVgap(10);
+	        pane.setHgap(10);
+	
+		
+		Button startButton = new Button();
+		
+		startButton.setText("Start");
+		node.getChildren().add(pane);
+			stage.show();
 			
 	}
 	//680x480
