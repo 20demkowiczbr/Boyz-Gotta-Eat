82e08a1f59a3280e3e3f0f476a6e23581ddeaf92
Bryce <19cullenbr@fpsct.org>
2018-05-31 08:12:39 -0400

    began levelselector

--- src/LevelSelector.java
+++ src/LevelSelector.java
@@ -1,4 +1,20 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.*;
 
 public class LevelSelector {
 
+	final Image map = new Image("asset/map.jpg");
+	
+	final ImageView mapNode = new ImageView();
+	mapNode.setImage(map);
 }
