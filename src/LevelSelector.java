fd63ed30034871627d48e7273c91e1d867935dcb
brandodemko <20demkowiczbr@fpsct.org>
2018-06-03 22:54:04 -0400

    Trying to change screen to arctic level

--- src/LevelSelector.java
+++ src/LevelSelector.java
@@ -1,5 +1,21 @@
-import com.sun.glass.ui.Application;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 
+import com.sun.glass.ui.Application;
+import com.sun.glass.ui.CommonDialogs.ExtensionFilter;
+import com.sun.glass.ui.CommonDialogs.FileChooserResult;
+import com.sun.glass.ui.Cursor;
+import com.sun.glass.ui.Pixels;
+import com.sun.glass.ui.Robot;
+import com.sun.glass.ui.Screen;
+import com.sun.glass.ui.Size;
+import com.sun.glass.ui.Timer;
+import com.sun.glass.ui.View;
+import com.sun.glass.ui.Window;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,7 +29,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
-public abstract class LevelSelector extends Application{
+public class LevelSelector extends Application{
 
 	public void start(Stage stage) throws Exception{
 	
@@ -26,15 +42,217 @@ public abstract class LevelSelector extends Application{
 		stage.setScene(scene);
 		
 		node.getChildren().addAll(mapNode);
+		
+		 Button arcticButton = new Button();
+			
+			arcticButton.setText("Arctic");
 
 		StackPane pane = new StackPane();
-		pane.getChildren().add(node);
+		pane.getChildren().addAll(node, arcticButton);
 		
 		//StackPane pane = new StackPane();
 		//pane.getChildren().addAll(mapNode);
-		
+		arcticButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+	           public void handle(ActionEvent event) {
+	               LevelSelector map = new LevelSelector();
+						try {
+							map.start(stage);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	           }
+	       });
 		stage.show();
+	
 		
 		
+		
+		
+		
+		
+	}
+	
+
+	@Override
+	protected void runLoop(Runnable launchable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void _invokeAndWait(Runnable runnable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void _invokeLater(Runnable runnable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Object _enterNestedEventLoop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void _leaveNestedEventLoop(Object retValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Window createWindow(Window owner, Screen screen, int styleMask) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Window createWindow(long parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public View createView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cursor createCursor(int type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cursor createCursor(int x, int y, Pixels pixels) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void staticCursor_setVisible(boolean visible) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Size staticCursor_getBestSize(int width, int height) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pixels createPixels(int width, int height, ByteBuffer data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pixels createPixels(int width, int height, IntBuffer data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pixels createPixels(int width, int height, IntBuffer data, float scale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int staticPixels_getNativeFormat() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Robot createRobot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected double staticScreen_getVideoRefreshPeriod() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected Screen[] staticScreen_getScreens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Timer createTimer(Runnable runnable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int staticTimer_getMinPeriod() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int staticTimer_getMaxPeriod() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected FileChooserResult staticCommonDialogs_showFileChooser(Window owner, String folder, String filename,
+			String title, int type, boolean multipleMode, ExtensionFilter[] extensionFilters, int defaultFilterIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected File staticCommonDialogs_showFolderChooser(Window owner, String folder, String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected long staticView_getMultiClickTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int staticView_getMultiClickMaxX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int staticView_getMultiClickMaxY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected boolean _supportsTransparentWindows() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean _supportsUnifiedWindows() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected int _getKeyCodeForChar(char c) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
