dbc885cb0c54d259c5b04e9829396ca01a10ba25
20demkowiczbr <20demkowiczbr@fpsct.org>
2018-06-04 07:58:25 -0400

    Some code in sprite class

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.image.Image;
 
 public class Sprite {
-	public Sprite() {
+	private List<Image> avatar;
+	
+	public Sprite(Image phase1, Image phase2, Image phase3) {
+		avatar = new ArrayList<Image>();
+		avatar.add(phase1);
+		avatar.add(phase2);
+		avatar.add(phase3);
 		
 		//random commit
 	}
