75c71261c79fb272c3cb638c00d9157d9b482ac4
brandodemko <20demkowiczbr@fpsct.org>
2018-06-04 16:17:47 -0400

    basic abstract level class that arctic class extends
    arctic class still needs its specific buttons added

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,22 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
-public class Level {
-
+public abstract class Level {
+	Image background;
+	
+	public Level(Image bg) {
+		background = bg;
+	}
+	
+	public void setPane() {
+		StackPane levelPane = new StackPane();
+		ImageView newBack = new ImageView();
+		newBack.setImage(getBG());
+	}
+	
+	public Image getBG() {
+		return background;
+	}
+	
 }
