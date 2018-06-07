3137310ed643268edc141fd72cdb738e5faaeb50
KevinKrupa <39557575+KevinKrupa@users.noreply.github.com>
2018-06-06 23:11:34 -0400

    Trying to upload image

--- src/Board.java
+++ src/Board.java
@@ -1,24 +1,45 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Board extends JPanel{
 	
-	Image background;
+	private Image background;
 	
-	public Board() {
+	/*public Board(String string) {
 		setFocusable(true);
 		ImageIcon i = new ImageIcon("asset/title.png");
 		background = i.getImage();
 	}
+	*/
 
-	public void paint(Graphics g) {
+	/*public void paint(Graphics g) {
 		super.paint(g);
 			Graphics2D g2d = (Graphics2D) g;
 			
 			g2d.drawImage(background, 0, 0, null);
 	}
-}
+*/
+	public Board(String fileName) {
+	    try {
+			background = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  }
+
+	  public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+
+	    // Draw the background image.
+	    g.drawImage(background, 0, 0, this);
+	  }
+	}
+
--- src/Frame.java
+++ src/Frame.java
@@ -1,21 +1,30 @@
+import java.awt.Button;
+import java.awt.GridBagLayout;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 public class Frame {
 	
-	public static void main(String[] args) {
+	private static Button button;
+	
+	public static void main(String[] args) throws IOException {
 		
 		JFrame frame = new JFrame();
 		frame.setTitle("Platformer");
-		frame.add(new Board());
+		//frame.add(new Board());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(544, 360);
-		frame.setVisible(true);
-		
+		frame.setLocationRelativeTo(null);
 		//frame.setLayout(new GridBagLayout());
-		//frame.setLocationRelativeTo(null);
-		//frame.setResizable(false);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	
+		//button = new Button("Click Me");
 		
+		//frame.add(button);
 		
+		frame.getContentPane().add(new Board("asset\\FinalTitle.jpg"));
 		
 	}
 	
