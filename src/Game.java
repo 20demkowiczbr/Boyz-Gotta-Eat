cf5bcdae934e7b56bca5edc4f877074dbcc53680
Bryce <39557620+19cullenbr@users.noreply.github.com>
2018-06-07 20:49:58 -0400

    resizing

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,35 @@
 import java.awt.Image;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class Game extends JPanel{
+public class Game extends JPanel implements ActionListener{
 
 	Player p;
 	Image img;
 	
-	public Game() {
+	public Game() 
+	{
 		p = new Player();
 		setFocusable(true);
-		ImageIcon i = new ImageIcon("asset/gorilla1.png");
+		ImageIcon i = new ImageIcon("asset/background.png");
 		img = i.getImage();
+		//sets timer to update player every 5 milliseconds
+		time = new Timer(5, this);
+		time.start();
 	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		repaint();
+	}
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+	}
+	
+	
 }
